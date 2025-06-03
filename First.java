a56347011714ceafa1584c9a78db777a50f68604
Chandra <chandrasekharkhatua234@gmail.com>
2025-06-03 11:01:44 +0530

    revision of dsa in java again

---
+++ First.java
@@ -0,0 +1,7 @@
+public class First
+{
+    public static void main(String args[])
+    {
+        System.out.println("hello chandra welcom eto your first step into java and be consistent again y.");
+    }
+}    
