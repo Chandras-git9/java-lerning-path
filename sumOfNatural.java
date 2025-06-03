6b380a8b868312bdc3487196f501aeca03b45929
Chandra <chandrasekharkhatua234@gmail.com>
2025-06-03 12:23:11 +0530

    sum of natural number

---
+++ sumOfNatural.java
@@ -0,0 +1,14 @@
+public class sumOfNatural {
+    public static void main(String args[])
+    {
+        // int j=5;
+        int sum=0;
+        for(int i=0; i<=5; i++)
+        {
+            sum=sum+i;
+        }
+        {
+            System.out.println(sum);
+        }
+    }
+}
