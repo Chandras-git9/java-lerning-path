da01b11e86c311133b34514b45dd09f806704acd
chandra <cs9050@gmail.com>
2025-07-04 10:09:00 +0530

    Initial commit

---
+++ ternary.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ternary {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number");
+        int n = sc.nextInt();
+        String 10
+        ans;
+        ans = (n % 2 == 0) ? "even" : "odd";
+        System.out.println(ans);
+    }
+}
