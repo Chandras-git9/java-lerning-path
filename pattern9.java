87c76f8e0810c05cc227ec84220511b29bbbdd1e
chandra <cs9050@gmail.com>
2025-07-05 11:52:46 +0530

    pattern learning

---
+++ pattern9.java
@@ -0,0 +1,21 @@
+public class pattern9 
+{
+    public static void main(String args[])
+    {
+        int rows = 5;
+        for (int i = rows; i >= 1; i--) 
+        {
+            // print spaces
+            for (int j = rows; j > i; j--) 
+            {
+                System.out.print(" ");
+            }
+            // print stars
+            for (int k = 1; k <= (2 * i - 1); k++) 
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
