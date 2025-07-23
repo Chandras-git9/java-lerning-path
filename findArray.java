11e5f9ac648b5e3c4268bcb9ced4fad6ba6b47dd
Chandra <chandrasekharkhatua234@gmail.com>
2025-07-23 15:15:28 +0530

    Add files via upload

---
+++ findArray.java
@@ -0,0 +1,13 @@
+public class findArray {
+    public static void main(String[] args) {
+        int arr[]={2,3,4};
+        for(int i=0; i<arr.length; i++)
+        {
+            if(arr[i]==4)
+            {
+                System.out.println("found at index: "+i);
+            }
+        }
+    }
+    
+}
---
+++ maxArray.java
@@ -0,0 +1,17 @@
+public class maxArray {
+    public static void main(String args[])
+    {
+        int ans=0;
+        int arr[]={2,5,3};
+        {
+            for(int i=0;i<arr.length; i++)
+            {
+                if(arr[i]>=5)
+                {
+                    System.err.println("found at index" +i);
+                }
+            }
+        }
+    }
+    
+}
